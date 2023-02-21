abf61c7d6e8e067f98671aaecf9ee3c8834164be
GageKolojaco <111771633+GageKolojaco@users.noreply.github.com>
2023-02-20 19:45:42 -0600

    added main method for testing

--- TestHW.java
+++ TestHW.java
@@ -1,3 +1,10 @@
 public class TestHW {
-    
+    public static void main(String[] args) {
+        MyArrayList arrayList = new MyArrayList();
+        System.out.println("Testing HW;");
+        System.out.println("Testing add:");
+        arrayList.add("Penis");
+        System.out.println(arrayList);
+        System.out.println(arrayList);
+    }
 }
